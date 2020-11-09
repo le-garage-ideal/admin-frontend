f59ec585a92582ea4f70553c9b7154114171b95a
Benjamin Caure <bcaure@gmail.com>
2020-11-09 13:56:50 +0100

    :wrench: configurable backend API

--- src/functions/api.js
+++ src/functions/api.js
@@ -1,6 +1,6 @@
 import { BehaviorSubject } from 'rxjs';
 
-const BASE_URL = process.env.BASE_URL || 'http://localhost:3000';
+const BASE_URL = process.env.REACT_APP_PUBLIC_URL || 'http://localhost:3000';
 
 export const currentUserObservable = new BehaviorSubject(null);
 
